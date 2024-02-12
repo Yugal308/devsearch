10716246d3687b6194f7360f4462cca55ea73a57
Yugal308 <yverma_bemba19@thapar.edu>
2024-02-13 00:46:26 +0530

    Added and render profiles

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=200, blank=True,null=True)
     email = models.CharField(max_length=500, blank=True,null=True)
     username = models.CharField(max_length=200, blank=True,null=True)
+    location = models.CharField(max_length=200, blank=True,null=True)
     short_intro = models.CharField(max_length = 500, blank=True,null=True)
     bio = models.TextField(blank=True,null=True)
     profile_image = models.ImageField(blank=True, null= True, default ='profiles/user-default.png', upload_to="profiles/")
@@ -20,4 +21,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+    
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True,null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+    
     
